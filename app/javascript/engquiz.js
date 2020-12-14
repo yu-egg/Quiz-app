8b51b4b37b08d9cc009fa872ddfd0513d58a5df3
yu-egg <69953750+yu-egg@users.noreply.github.com>
2020-12-14 10:57:30 +0900

    Update engquiz.js

--- app/javascript/engquiz.js
+++ app/javascript/engquiz.js
@@ -24,7 +24,6 @@ window.addEventListener("DOMContentLoaded", () =>{
       {q: '自動詞は？', c: ['Daniel smiled at Nancy.','She kissed him gently.']},
       {q: '他動詞は？', c: ['She kissed him gently.', 'Daniel smiled at Nancy.']},
       {q: '他動詞は？', c: ['She kissed him gently.', 'Daniel smiled at Nancy.']},
-      {q: '他動詞は？', c: ['She kissed him gently.', 'Daniel smiled at Nancy.']},
 
     ]);
     let currentNum = 0;
